99a1a8b3a24b3a4e151f116d59f85b88994e6470
Samuel Odundo <samieodd@gmail.com>
2020-02-25 23:32:31 +0300

    Fixed ui misalignments, added class to launch ui

---
+++ MainWindow.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'mainwindow.ui'
+#
+# Created by: PyQt5 UI code generator 5.14.0
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(275, 332)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(30, 29, 190, 16))
+        self.label.setObjectName("label")
+        self.radioButton = QtWidgets.QRadioButton(self.centralwidget)
+        self.radioButton.setEnabled(False)
+        self.radioButton.setGeometry(QtCore.QRect(30, 47, 92, 17))
+        self.radioButton.setObjectName("radioButton")
+        self.radioButton_2 = QtWidgets.QRadioButton(self.centralwidget)
+        self.radioButton_2.setGeometry(QtCore.QRect(128, 47, 94, 17))
+        self.radioButton_2.setObjectName("radioButton_2")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(171, 70, 75, 23))
+        self.pushButton.setObjectName("pushButton")
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(30, 71, 133, 20))
+        self.lineEdit.setText("")
+        self.lineEdit.setObjectName("lineEdit")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(30, 110, 106, 16))
+        self.label_2.setObjectName("label_2")
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_2.setGeometry(QtCore.QRect(30, 129, 133, 20))
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(171, 128, 75, 23))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.checkBox = QtWidgets.QCheckBox(self.centralwidget)
+        self.checkBox.setGeometry(QtCore.QRect(30, 157, 145, 17))
+        self.checkBox.setObjectName("checkBox")
+        self.checkBox_2 = QtWidgets.QCheckBox(self.centralwidget)
+        self.checkBox_2.setGeometry(QtCore.QRect(30, 180, 136, 17))
+        self.checkBox_2.setObjectName("checkBox_2")
+        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_3.setGeometry(QtCore.QRect(70, 250, 141, 31))
+        self.pushButton_3.setObjectName("pushButton_3")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 275, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label.setText(_translate("MainWindow", "Choose where data will be loaded from:"))
+        self.radioButton.setText(_translate("MainWindow", "Google Sheets"))
+        self.radioButton_2.setText(_translate("MainWindow", "Local Excel File"))
+        self.pushButton.setText(_translate("MainWindow", "Browse"))
+        self.label_2.setText(_translate("MainWindow", "Choose output folder:"))
+        self.pushButton_2.setText(_translate("MainWindow", "Browse"))
+        self.checkBox.setText(_translate("MainWindow", "Continue from last record"))
+        self.checkBox_2.setText(_translate("MainWindow", "Open folder when done"))
+        self.pushButton_3.setText(_translate("MainWindow", "Generate 837p Forms"))
--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 import claim_creator
+from PyQt5 import QtWidgets, uic
+from MainWindow import Ui_MainWindow
+import sys
 
 # run the claim creator function
-claim_creator.claim_generator()
+# claim_creator.claim_generator()
+
+launch_folder = False
+
+
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self, *args, obj=None, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        self.setupUi(self)
+
+
+app = QtWidgets.QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+app.exec()
