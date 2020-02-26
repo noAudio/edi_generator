ba052d6ad58adb4fbc7a6b338333297e2d5d346d
Samuel Odundo <samieodd@gmail.com>
2020-02-26 17:45:07 +0300

    Added functionality to the buttons

--- main.py
+++ main.py
@@ -7,29 +7,50 @@ import sys
 # run the claim creator function
 # claim_creator.claim_generator()
 
-launchfolder = False
-
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    launchFolder = False
+
     def __init__(self, *args, obj=None, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
+        self.setupUi(self)
+        # run the claim generation script
+
+        # browse for file and folder location
+        self.btnAddFileLocation.clicked.connect(self.pickFile)
+        self.btnOutputFolder.clicked.connect(self.pickFolder)
+        launchFolder = self.chkOpenOuputFolder.clicked.connect(
+            self.launchFolderWhenComplete)
         # assign path to excel file and path to output folder
         #  to variables
         excelpath = self.txtExcelLocation.text()
         outputfolder = self.txtOutputLocation.text()
-        # check whether to launch folder after claim generation
-        if self.chkOpenOuputFolder == True:
-            launchfolder = True
 
-        # run the claim generation script
-
-        self.setupUi(self)
-
-    def pick_folder(self):
+    def pickFile(self):
         fileName, _ = QFileDialog.getOpenFileName(
             self, 'QFileDialog.getOpenFileName()', '', 'All Files (*);;Excel Files (*.xlsx)')
+        self.txtExcelLocation.setText(fileName)
         if fileName:
             print(fileName)
+
+    def pickFolder(self):
+        folderName = str(QFileDialog.getExistingDirectory(
+            self, "Select Directory"))
+        self.txtOutputLocation.setText(folderName)
+        if folderName:
+            print(folderName)
+
+    def launchFolderWhenComplete(self):
+        launchfolder = 'Unassigned'
+      # check whether to launch folder after claim generation
+        if self.chkOpenOuputFolder.isChecked():
+            launchfolder = True
+            print(launchfolder)
+        else:
+            launchfolder = False
+            print(launchfolder)
+
+        return launchfolder
 
 
 app = QtWidgets.QApplication(sys.argv)
